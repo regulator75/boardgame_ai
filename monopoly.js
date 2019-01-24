302a671c84416aac37c0922c4e2dd674dfeafa89
Viktor Mårtensson <viktor.martensson@gmail.com>
2019-01-24 01:06:50 -0800

    Got the basic call rolling. A ton of APIs implemented, basic walking around debugged.

--- monopoly.js
+++ monopoly.js
@@ -4,12 +4,10 @@
 // Player#1 AI
 //
 simple_player_ai = {
-	act: function(game) {
-		// Repeatedly called when its your turn until you call
-		// game.rolldice()
+	turn: function(game) {
+		// Called when its your turn. End your turn and roll the dice by returning from this function.
 
 		// APIs for actions in "game"
-		// game.rolldice() - Rolls the dice, your turn is over (unless you roll two identical)
 		// game.buyhouses([slots]) - Buys one house on each property in the array. All purchases will fail if not enough funds
 		// game.sellhouses([slots]) - Sells one house on each property in the array. If sale would make house-status unlawfull all sales fail.
 		// game.playgetoutofjailcard() - Next time you roll, if you are in jail, you will get out. Can be played before getting sent to jail.
@@ -24,7 +22,7 @@ simple_player_ai = {
 		// game.player(i) - returns information about a player
 		// game.player(i).money() - How much money do this player have
 		// game.player(i).properties() - returns a vector of the owned properties of this player
-		// game.player(i).playgetoutofjailcard() - How many get-out-of-jail cards do this player have.
+		// game.player(i).playgetoutofjailcards() - How many get-out-of-jail cards do this player have.
 
 
 		// APIs for making trades
@@ -38,9 +36,14 @@ simple_player_ai = {
 		//   To make it happen you must call commit_trade.
 		//   If the trade would break a series that have houses on them the trade will 
 		//   never be offered to the other party and the return value will be 0
-		//   Trade IDs are expired when the "act" function returns.
+		//   Trade IDs are expired when the "turn" function returns.
 		// 
 		// game.commit_trade(tradeid)
+
+		//
+		// API for getting informatoin about property
+		// game.property(slot)
+		// 
 	},
 
 	consider_trade: function(game, offering, want) {
@@ -54,15 +57,190 @@ simple_player_ai = {
 		// You have landed on a piece of land 
 		// that is not owned, this is your chance to buy it
 		// Return "true" if you want to buy it.
-		// This function will not be called if you cant afford it
+		// If you do not have the money to buy it, you can use the mortage, sellhouses, and trade APIs to secure money
+		// 
 	},
 
 	consider_choice: function(game, option) {
 		// Called when you pull a card where you need to make a decision.
 		// The option is from the enumeration CHOICES_X
+	},
+
+	raise_money: function(game, lacking_funds) {
+		// You need to raise money because of landing on something costly.
+		// You can access the trade API, as well as selling houses or mortageing properties.
+		// lacking_funds is the positive value of what the ai 
+		// would see from calling "money()"
 	}
 }
 
+function RollDice() {
+	return Math.floor(Math.random() * 6) + 1;
+}
+
+function tick() {
+
+	player = players[next_player]
+
+	game.set_active_player(player)
+	player.ai.turn(game)
+	game.expunge_trade_records() // dont leave lingering trades
+	var last = double_rolls_so_far == 2;
+
+	if( game._rolldice(last) && !last) {
+		double_rolls_so_far++;
+	} else {
+		next_player++;
+		next_player%=players.length
+		double_rolls_so_far = 0;
+	}
+
+	return true;
+}
+
+function MakeGame() {
+	return {
+		current_player : null,
+		set_active_player: function(ap) { this.current_player = ap},
+
+		// API offered to AI
+		slot: function() { return this.current_player.slot() },
+		money: function() { return current_player.money() },
+		properties: function() { return current_player.properties() }, // TODO make deep copy.
+
+
+		playgetoutofjailcard: function() {
+			if(current_player.gojcards > 0 && this.current_player.slot() == 40 ) { // have card, is in jail.
+				current_player.gojcards--;
+				_gotoslot(10);
+			}
+		},
+
+		// Trades
+		trades_offered : [],
+		_create_trade_record : function(other_player, offer,want) {
+			this.trades_offered.push({player:other_player, offer: offer, want: want, valid: true})
+		},
+		expunge_trade_records : function() {
+			this.trades_offered.forEach(function(t){t.valid = false}) // Make them invalid incase someone will try to exeecute them later.
+			this.trades_offered = []
+		},
+
+		// Moves
+
+		_rolldice: function(last) {
+			// TODO figure out how to get money out if they tried to roll three times to get out of jail and failed.
+			d1 = RollDice()
+			d2 = RollDice()
+			if(last && d1==d2) {
+				this._gotoslot(40) //jail
+
+			} else if(this.current_player.slot() == 40 && d1 == d2){ // Currently in jail
+				// Lucky roll, got out
+				this._gotoslot(10+d1+d2) //10 is the "visiting jail" slot
+			} else {
+				var newslot = this.current_player.slot() + d1 + d2
+				if(newslot > 39) { // Passing or landing on GO
+					newslot -= 40
+					this.current_player._money += 200 // COLLECT 200 WHEN PASSING GO
+				}
+
+				this._gotoslot(newslot)
+			}
+			return d1==d2
+
+		},
+		_gotoslot : function(slot) {
+			// Is this a property owned by someone
+
+			this.current_player._slot = slot;
+			prop = all_slots[slot]
+
+			var rent = 0;
+			var recieving_player = null;
+			// If there is rent but no recieving player it goes to the bank, 'cause taxes.
+
+			switch(all_slots[slot].type) {
+				case TYPE_LAND:
+					// Check if we need to pay
+					if(prop.owner != null && prop.owner != this.current_player) {
+						recieving_player = prop.owner 
+						// Need to pay...
+						rent = CalculateRent(slot)
+					} else if(prop.owner == null) {
+						// No-one ownes this slot, lets see if the AI wants to buy it
+						want_to_buy = this.current_player.ai.consider_purchase(this, slot)
+
+						if(want_to_buy && prop.price <= this.current_player._money) {
+							// Its a DEAL!
+							prop.owner = this.current_player
+							this.current_player._money -= prop.price
+						}
+					}
+					break;
+				case TYPE_JAILED:
+				case TYPE_FREEPARKING:
+				case TYPE_JAILVISIT:
+					// Nothing
+					break;
+				case TYPE_GO:
+					// Nothing by landing here. Payment for passing go
+					// is calculated elsewhere
+					break;
+				case TYPE_GOTOJAIL:
+					// No cost, just update location.. 
+					player._slot = 40
+					break;
+				case TYPE_TAX:
+					rent = prop.cost
+					break;
+				case TYPE_CHANCE:
+					// TODO Add cards and shit
+					break;
+				case TYPE_COMMUNITY:
+					// TODO Add cards and shit
+					break;
+
+
+			}
+
+
+			if(rent > 0){
+
+				if(this.current_player._money < rent) {
+					// Player need to raise money
+
+					this.current_player.raise_money(this, rent - this.current_player._money)
+
+					// If the player could not raise the money, out of business.
+					if(this.current_player._money < rent) {
+						// BANKRUPT!
+						if(recieving_player != null)
+							recieving_player._money += current_player._money; // Gets all his cash
+						this.current_player._money -= rent; // Should land in negative territory
+					} else {
+						// Player recovered
+						if(recieving_player != null)
+							recieving_player._money += rent; 
+						this.current_player._money -= rent; // Should stay in positive territory
+					}
+				} else {
+					// simply give the other player the money
+					this.current_player._money -= rent
+
+					if(recieving_player) {
+						recieving_player._money += rent
+					}
+				}
+			}
+		},
+
+
+
+
+
+	}
+}
 
 // Function card_option should return "false" for the first option and "true" for second. 
 var CHOICES_CHANCE_OR_PAY=0
@@ -76,6 +254,9 @@ var CHOICES_CHANCE_OR_PAY=0
 //
 var properties = [];
 var players = [];
+var game = null;
+var next_player = 0;
+var double_rolls_so_far = 0;
 
 function Rect(x,y,w,h) {
 	return {
@@ -86,14 +267,33 @@ function Rect(x,y,w,h) {
 	}
 }
 
+var colorcount = 0
+var colors_of_players = [
+"#777777",
+"#ff00ff",
+"#ff0000",
+"#000000",
+"#00ffff",
+"#ffff00"]
+function GetNextColor() {
+	colorcount++
+	colorcount%=colors_of_players.length
+	return colors_of_players[colorcount]
+
+}
+
 function MakePlayer(ai) {
 	return {
-		spot: 0,
-		money: 2000,
-		marker_color: "#00ff00",
-		properties: [],
-		gojcards: 0,
-		ai:ai
+		_slot: 0,
+		_money: 2000,
+		marker_color: GetNextColor(),
+		_gojcards: 0,
+		ai:ai,
+		money:       function() { return this._money },
+		slot:        function() { return this._slot  },
+		properties:  function() { return all_slots.filter(property => property.owner == this) },
+		playgetoutofjailcards:    function() { return this._gojcards  }
+
 	}
 }
 
@@ -118,7 +318,8 @@ function MakeBuyableProperty(slot, name, price, houseprice, rent, family) {
 		slot: slot,
 		rent: rent, // empty, 1,2,3,4 houses, hotel
 		family: family, // brown, blue, pink etc. "rail" means its a railroad, "company" its the electricity of waterworks.
-		houses: 0 // 5 is hotel
+		houses: 0, // 5 is hotel
+		owner: null
 	}
 	all_slots[slot] = prop;
 }
@@ -173,8 +374,8 @@ function DrawBoard(){
 	}
 }
 
-function GetPlayersOnSpot(spot) {
-	return players.filter(function(p) { return p.spot == spot })
+function GetPlayersOnSpot(slot) {
+	return players.filter(function(p) { return p.slot() == slot })
 }
 
 function UI_GetLocationsForSpotAndCount(spot, playercount, spacing, trans_x, trans_y) {
@@ -218,6 +419,7 @@ function DrawPlayerPieces(ctx){
 
 	// each other.
 	for(var i = 0 ; i < 41 ; i++) { // 40 spots on the board + 1 for the Jail
+		for(p in players) { ""+p+" " +console.log(players[p].slot) ;}
 		var players_here = GetPlayersOnSpot(i);
 		var marker_locations = UI_GetLocationsForSpotAndCount(i,players_here.length,0.33, 0,0);
 		for(x = 0 ; x < players_here.length ; x++) {
@@ -441,6 +643,56 @@ function UI_GeneratePlayerStatuses() {
 
 }
 
+function GetPropertiesInFamily(f) {
+	return all_slots.filter(p => p.family = f)
+}
+
+function CountOwnedInFamily(player,f) {
+	return GetPropertiesInFamily(f).filter(p => p.owner == player)
+}
+
+
+// Assumes
+//  1. Its a property that you can build houses on (TYPE_LAND)
+//  2. The property have an owner.
+function CalculateRent(slot) {
+	var p = all_slots[slot]
+
+	// All paths must set "rent"
+	var rent = null
+
+	if(p.family == FAMILY_RAILROAD) {
+		// Depending on how many railroads the same player owns
+		// the rent is different.
+		cnt = CountOwnedInFamily(p.owner,FAMILY_RAILROAD)
+		rent = p.rent[cnt]; //
+
+	} else if(p.family == FAMILY_COMPANY) {
+		// If one propertiy is owned, 
+		dices = RollDice() + RollDice() // TODO this is not supposed to be a re-roll, the last rolled dice is what should be used.
+		cnt = CountOwnedInFamily(p.owner,FAMILY_COMPANY)
+		rent = p.rent[cnt-1] * dices
+	} else {
+
+		// Plain old property
+
+		// is there any buildings on the slot?
+		if(p.houses > 0) {
+			rent = p.rent[p.houses]
+		}
+		// No building, but maybe same owner for everythign in the family?
+		else if(CountOwnedInFamily(p.owner,p.family).length == GetPropertiesInFamily(p.family).length) {
+			// same owner, double the price
+			rent = p.rent[0] * 2
+		} else {
+			// Just plain rent
+			rent = p.rent[0]
+		}
+	}
+
+	return rent
+}
+
 var FAMILY_BROWN     = "brown"
 var FAMILY_LIGHTBLUE = "light blue"
 var FAMILY_PINK      = "pink"
@@ -487,14 +739,14 @@ function MakeProperties() {
     MakeBuyableProperty(39, "Boardwalk",             400,200,[50,100,200,600,1400,1700,2000],FAMILY_ROYALBLUE);
 
         // Railroads
-    MakeBuyableProperty( 5, "Reading Railroad",      100,null,null,FAMILY_RAILROAD);
-    MakeBuyableProperty(15, "Pennsylvania Railroad", 100,null,null,FAMILY_RAILROAD);
-    MakeBuyableProperty(25, "B&O Railroad",          100,null,null,FAMILY_RAILROAD);
-    MakeBuyableProperty(35, "Short Line",            100,null,null,FAMILY_RAILROAD);
+    MakeBuyableProperty( 5, "Reading Railroad",      100,null,[25,50,100,200],FAMILY_RAILROAD); // rent is special
+    MakeBuyableProperty(15, "Pennsylvania Railroad", 100,null,[25,50,100,200],FAMILY_RAILROAD); // rent is special
+    MakeBuyableProperty(25, "B&O Railroad",          100,null,[25,50,100,200],FAMILY_RAILROAD); // rent is special
+    MakeBuyableProperty(35, "Short Line",            100,null,[25,50,100,200],FAMILY_RAILROAD); // rent is special
 
     // Verk
-    MakeBuyableProperty(12, "Electric company",      100,null,null,FAMILY_COMPANY);
-    MakeBuyableProperty(28, "Water works",           100,null,null,FAMILY_COMPANY);
+    MakeBuyableProperty(12, "Electric company",      100,null,[4,10],FAMILY_COMPANY); // rent is special
+    MakeBuyableProperty(28, "Water works",           100,null,[4,10],FAMILY_COMPANY); // rent is special
 
 
     MakeCardProperty(2,  TYPE_COMMUNITY)
@@ -517,44 +769,6 @@ function MakeProperties() {
 
 
     // Add fake houses
-    AddHouseToSlot(6);
-    AddHouseToSlot(6);
-    AddHouseToSlot(6);
-
-    AddHouseToSlot(8);
-    AddHouseToSlot(8);
-
-    AddHouseToSlot(9);
-
-    AddHouseToSlot(16);
-    AddHouseToSlot(16);
-    AddHouseToSlot(16);
-    AddHouseToSlot(16);
-    AddHouseToSlot(16);
-
-    AddHouseToSlot(18);
-    AddHouseToSlot(18);
-
-    AddHouseToSlot(19);   
-
-    AddHouseToSlot(26);
-    AddHouseToSlot(26);
-    AddHouseToSlot(26);
-
-    AddHouseToSlot(27);
-    AddHouseToSlot(27);
-
-    AddHouseToSlot(29);   
-
-
-    AddHouseToSlot(37);
-    AddHouseToSlot(37);
-    AddHouseToSlot(37);
-    AddHouseToSlot(37);
-    AddHouseToSlot(37);    
-
-    AddHouseToSlot(39);           
-
 
 	// Chance-slots
 }
@@ -573,35 +787,66 @@ function MakeProperties() {
 //}
 //}
 
+naive_ai = {
+	turn: function(game) {
 
+	},
 
+	consider_trade: function(game, offering, want) {
+		return true; // SURE we take ANY TRADE
+	},
+
+	consider_purchase: function(game, slot) {
+		return true; // IM BUYING EVERYTHING
+	},
+
+	consider_choice: function(game, option) {
+		return true; // I DONT KNOW BUT LEFT IS COOL
+	},
+
+	raise_money: function(game, lacking_funds) {
+		// Just fail..
+	}
+}
+function runner() {
+
+	var runFast = false;//document.getElementById("fastrun").checked
+	var keepOnRunning = true;//document.getElementById("autorun").checked
+
+	if( tick() ) {	
+		
+		if(keepOnRunning) {		
+			var delayToNextFrame;
+			if(runFast) {
+				delayToNextFrame = 10;
+			} else {
+				delayToNextFrame = 50
+			}
+			window.setTimeout(runner, delayToNextFrame);
+		}
+	} else {
+		// Game over. Restart
+		//ResetStateAndLoadRobots();
+		//runner();
+	}
+	DrawBoard();
+}
 function OnLoad() {
-	players.push(MakePlayer(null))
-	players.push(MakePlayer(null))
-	players.push(MakePlayer(null))
-	players.push(MakePlayer(null))
-	players.push(MakePlayer(null))
-	players.push(MakePlayer(null))
 
-	players[0].marker_color = "#777777"
-	players[1].marker_color = "#ff00ff"
-	players[2].marker_color = "#ff0000"
-	players[3].marker_color = "#000000"
-	players[4].marker_color = "#00ffff"
-	players[5].marker_color = "#ffff00"
-
-
-	players[0].spot=0
-	players[1].spot=40	
-	players[2].spot=6
-	players[3].spot=6	
-	players[4].spot=15
-	players[5].spot=10 
+	game = MakeGame()
+	players.push(MakePlayer(naive_ai))
+	players.push(MakePlayer(naive_ai))
+	players.push(MakePlayer(naive_ai))
+	players.push(MakePlayer(naive_ai))
 
 	MakeProperties()
 
 	// Draw current board
 	CalculatePieceLocations(document.getElementById("myCanvas"));
 	DrawBoard();
+
+	runner();
+
+
 
 }
