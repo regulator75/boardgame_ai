e3ef24b7b9b55290286abb6234fa4e1a2a780526
Viktor Mårtensson <viktor.martensson@gmail.com>
2019-02-05 20:04:01 -0800

    Adding description for all community chest and chance cards

---
+++ cards.js
@@ -0,0 +1,40 @@
+Chans:
+
+Advance to Boardwalk
+Building loan is due. Collect 150
+Advance to nearest railroad. If owned by someone else pay double rent
+Go to go and collect 200
+Speeding ticket 15
+Go to jail without passing go
+Advance to nearest railroad. If owned by someone else pay double rent
+Advance to Reading Railroad. Collect 200 if pass go
+You have been elected chariman of the board, pay 50 to each player
+You get a 50 return from the bank
+GET OUT OF JAIL FREE. Keep until used
+Go 3 steps back
+Advance to St. Charles Place, if you pass go collect 200
+House maintanence, Pay 25 per house and 100 per hotel
+Advance to nearest utility. Pay 10 times the dice roll if owned.
+Advance to Illinois Avenue. If pass go collect 200
+
+
+
+Community Chest:
+
+Its your birthday, collect 10 from each player
+School fees, pay 50
+You inherit 100
+Tax return, collect 200
+You sell shares, collect 50
+GET OUT OF JAIL FREE, keep until used
+Hospital bill, pay 100
+You get taxed for street repairs, 40 per house and 115 per hotel
+You won second price in a beauty contest, collect 10
+Proceed to GO
+Bank made a mistake in your favor, collect 200
+GO TO JAIL
+Return on life insurance, collect 100
+Vacation savings gains value, collect 100
+Income from consulting, collect 25
+Doctors fee, collect 50
+
